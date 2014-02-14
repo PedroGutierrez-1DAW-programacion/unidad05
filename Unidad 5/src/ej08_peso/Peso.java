32a4a4ca1e713daf95927e51a8d51d29dd84b25d
Pedro-vk <pedrogp9_6_93@hotmail.com>
2014-02-14 17:21:19 +0100

    Ejercicio 6 - simplificación de la clase Peso

--- Unidad 5/src/ej08_peso/Peso.java
+++ Unidad 5/src/ej08_peso/Peso.java
@@ -2,24 +2,30 @@ package ej08_peso;
 
 public class Peso {
 	private double pesoKG;
-
-	private final double kgLibra	=	2.20462262;
-	private final double kgLingote	=	0.06854009;
-	private final double kgOnza		=	35.2739619;
-	private final double kgPenique	= 	645.161290;
-	private final double kgGramos	=	1000.00000;
-	private final double kgQuintal	= 	0.01000000;
 	
 	public Peso(double peso, String unidad){
-		// Si la unidad es errónea devuelve usa el Kg
+		// Si la unidad es errónea usa el Kg como unidad
 		this.pesoKG = peso * (1 / getPesoPorKilo(unidad));
 	}
 
-	public double getPeso(String unidad){
+	private final double getPesoPorKilo(String unidad){
+		switch(unidad.toLowerCase()){
+			default:
+			case "kg":	return 1.00000000; // Kilogramos
+			case "lb":	return 2.20462262; // Libras
+			case "li":	return 0.06854009; // Lingotes
+			case "oz":	return 35.2739619; // Onzas
+			case "p":	return 645.161290; // Peniques
+			case "g":	return 1000.00000; // Gramos
+			case "q":	return 0.01000000; // Quintales
+		}
+	}
+
+	public double getPeso(String unidad){ // Peso con dos decimales
 		return getPeso(unidad, 2);
 	}
 	
-	public double getPeso(String unidad, int decimales){
+	public double getPeso(String unidad, int decimales){ // Peso con los decimales establecidos
 		return redondeo(this.pesoKG * getPesoPorKilo(unidad), decimales);
 	}
 
@@ -30,23 +36,9 @@ public class Peso {
 	public double getLingotes(){
 		return getPeso("li");
 	}
-
-	private double getPesoPorKilo(String unidad){
-		switch(unidad.toLowerCase()){
-			case "lb": return kgLibra;
-			case "li": return kgLingote;
-			case "oz": return kgOnza;
-			case "p": return kgPenique;
-			case "kg": return 1;
-			case "g": return kgGramos;
-			case "q": return kgQuintal;
-		}
-		return 1;
-	}
 	
 	private double redondeo(double n, int decimales){
 		double mult = Math.pow(10, decimales);
 		return Math.round(n * mult) / mult;
-	}
-	
+	}	
 }
