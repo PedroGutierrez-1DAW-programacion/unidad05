e81c7dd403e0c2516e4f5875561153a695cfe7e5
Pedro-vk <pedrogp9_6_93@hotmail.com>
2014-02-21 16:33:16 +0100

    Actualizado ejercicio números racionales - simplifica en cada acción

--- Unidad 5/src/ej10_numerosRacionales/Racional.java
+++ Unidad 5/src/ej10_numerosRacionales/Racional.java
@@ -23,32 +23,45 @@ public class Racional {
 	}
 	
 	public String toString(){
-		simplificar();
 		return this.nominador + "/" + this.denominador;
 	}
-	public String toFloatString(){
-		return String.valueOf(this.nominador/1.0/this.denominador);
+	public float toFloat(){
+		return (float) (this.nominador/1.0/this.denominador);
 	}
 	
 	public void suma(Racional numero){
 		int oNumerador = numero.getDenominador() * this.nominador;
 		int nNumerador = this.denominador * numero.getNominador();
-		this.nominador = oNumerador + nNumerador;
-		this.denominador *= numero.getDenominador();
+		setNomDen(
+			oNumerador + nNumerador,
+			this.denominador * numero.getDenominador()
+		);
 	}
 	public void resta(Racional numero){
 		int oNumerador = numero.getDenominador() * this.nominador;
 		int nNumerador = this.denominador * numero.getNominador();
-		this.nominador = oNumerador - nNumerador;
-		this.denominador *= numero.getDenominador();
+		setNomDen(
+			oNumerador - nNumerador,
+			this.denominador * numero.getDenominador()
+		);
 	}
 	public void multiplicar(Racional numero){
-		this.nominador *= numero.getNominador();
-		this.denominador *= numero.getDenominador();
+		setNomDen(
+			this.nominador * numero.getNominador(),
+			this.denominador * numero.getDenominador()
+		);
 	}
 	public void dividir(Racional numero){
-		this.nominador *= numero.getDenominador();
-		this.denominador *= numero.getNominador();
+		setNomDen(
+			this.nominador * numero.getDenominador(),
+			this.denominador * numero.getNominador()
+		);
+	}
+	
+	private void setNomDen(int nominador, int denominador){
+		this.nominador = nominador;
+		this.denominador = denominador;
+		simplificar();
 	}
 	
 	private void simplificar(){
